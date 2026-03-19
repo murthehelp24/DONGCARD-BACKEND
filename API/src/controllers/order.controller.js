826da39e3b19d3a120e1399776641c69227c03d3
murthehelp24 <attachaijumpahom@gmail.com>
2026-03-19 20:07:36 +0700

    order success

--- API/src/controllers/order.controller.js
+++ API/src/controllers/order.controller.js
@@ -1,35 +1,76 @@
 import createError from 'http-errors'
-import { createOrder, findAllOder, findOrderById } from "../services/order.service.js"
+import { createOrder, findOrderById, getAllOrders, getUserOrders, updatePayment, updateStatus } from "../services/order.service.js"
 
 
 export async function addOrder(req, res, next) {
   try {
-    console.log(req.body)
-    const orders = await createOrder(req.body)
+    const { address, items } = req.body
+    const userId = req.user.id
+    const order = await createOrder(userId, address, items)
     res.json({
-      message: "เพิ่มออเดอร์สำเร็จ",
-      orders
+      message: "สร้างออเดอร์สำเร็จ",
+      order
     })
   } catch (error) {
     next(error)
   }
 }
 
-export async function getAllOrder(req, res, next) {
+export async function getMyOrders(req, res, next) {
   try {
-    const orders = await findAllOder()
-    res.json({ orders })
+    const orders = await getUserOrders(req.user.id)
+    res.json(orders)
   } catch (error) {
     next(error)
   }
 }
 
-export async function getOrderById(req, res, next) {
+export async function getOrderDetail(req, res, next) {
   try {
-    const { id } = req.params
-    const order = await findOrderById(id)
+    const order = await findOrderById(req.params.id)
     if (!order) throw createError(404, 'ไม่พบออเดอร์นี้')
-    res.json({ order })
+
+    // เช็คว่าเป็นเจ้าของหรือ admin
+    if (req.user.role !== "ADMIN" && order.buyerId !== req.params.id) {
+      console.log(req.user.role)
+      throw createError(403, 'ไม่มีสิทธ์เข้าถึง')
+    }
+    res.json(order)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export async function notifyPayment(req, res, next) {
+  try {
+    // รอทำ frontend ถึงมาแก้ path
+    const slipUrl = req.file ? req.file.path : req.body.paymentSlip
+    if (!slipUrl) throw createError(400, 'กรุณาแนบสลิป')
+
+    const order = await updatePayment(req.params.id, slipUrl)
+    res.json({ message: 'แจ้งโอนเงินสำเร็จ', order })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export async function adminUpdateStatus(req, res, next) {
+  try {
+    const { status } = req.body
+    const order = await updateStatus(req.params.id, status)
+    res.json({
+      message: `เปลี่ยนสถานะเป็น ${status} สำเร็จ`,
+      order
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export async function adminGetAllOrder(req, res, next) {
+  try {
+    const orders = await getAllOrders()
+    res.json(orders)
   } catch (error) {
     next(error)
   }
--- API/src/routes/order.route.js
+++ API/src/routes/order.route.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import { addOrder, getAllOrder, getOrderById } from '../controllers/order.controller.js'
+import { addOrder, adminGetAllOrder, adminUpdateStatus, getMyOrders, getOrderDetail, notifyPayment, } from '../controllers/order.controller.js'
+import { adminCheck, authenticate } from '../middlewares/auth.middleware.js'
+import upload from '../middlewares/upload.middleware.js'
 
 
 const router = express.Router()
+// admin แก้ไข status 
+router.patch('/:id/status', authenticate, adminCheck, adminUpdateStatus)
+router.get('/admin/all', authenticate, adminCheck, adminGetAllOrder)
 
-router.post('/', addOrder)
-router.get('/', getAllOrder)
-router.get('/:id', getOrderById)
+router.post('/', authenticate, addOrder)
+router.get('/', authenticate, getMyOrders)
+router.get('/:id', authenticate, getOrderDetail)
+router.patch('/:id', authenticate, upload.single('paymentSlip'), notifyPayment)
+
 
 export default router 
